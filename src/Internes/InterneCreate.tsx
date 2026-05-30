df4bd7a5c6989da9383bd9772b9defac4b21a78b
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-30 09:47:47 +0300

    Add InterneCreate component for creating new interns with form validation

---
+++ src/Internes/InterneCreate.tsx
@@ -0,0 +1,42 @@
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  NumberInput,
+  SelectInput,
+  BooleanInput,
+  required,
+  minValue,
+  email,
+} from "react-admin";
+
+const departmentChoices = [
+  { id: "Informatique", name: "Informatique" },
+  { id: "Marketing", name: "Marketing" },
+  { id: "RH", name: "RH" },
+];
+
+export const InterneCreate = () => (
+  <Create redirect="list">
+    <SimpleForm>
+      <TextInput source="firstName" label="Prénom" validate={required()} />
+      <TextInput source="lastName" label="Nom" validate={required()} />
+      <TextInput
+        source="email"
+        label="Email"
+        validate={[required(), email()]}
+      />
+      <SelectInput
+        source="department"
+        label="Département"
+        choices={departmentChoices}
+        validate={required()}
+      />
+      <NumberInput
+        source="salary"
+        label="Salaire"
+        validate={[required(), minValue(0)]}
+      />
+    </SimpleForm>
+  </Create>
+);
