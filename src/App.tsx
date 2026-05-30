60ffa2dcc5a53cd51c328193ce7d75666ebdcb7c
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-30 09:45:15 +0300

    Add InterneList component and integrate it into the Admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Create, ListGuesser, Resource } from "react-admin";
 import { Layout } from "./Layout";
 import jsonServerProvider from "ra-data-json-server";
 import { EmployeeList } from "./employees/EmployeeList";
 import { EmployeeEdit } from "./employees/EmployeeEdit";
 import { EmployeeShow } from "./employees/EmployeeShow";
 import { EmployeeCreate } from "./employees/EmployeeCreate";
+import { InterneList } from "./Internes/InterneList";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
@@ -17,5 +18,8 @@ export const App = () => (
       edit={EmployeeEdit}
       show={EmployeeShow}
     />
+    <Resource
+    name="internes"
+    list={InterneList}/>
   </Admin>
 );
---
+++ src/Internes/InterneList.tsx
@@ -0,0 +1,25 @@
+
+import { BooleanField, DataTable, DateField, EmailField, List, ReferenceField } from 'react-admin';
+
+export const InterneList = () => (
+    <List>
+        <DataTable>
+            <DataTable.Col source="id" />
+            <DataTable.Col source="firstname" />
+            <DataTable.Col source="lastname" />
+            <DataTable.Col source="email">
+                <EmailField source="email" />
+            </DataTable.Col>
+            <DataTable.Col source="mentorId">
+                <ReferenceField source="mentorId" reference="mentors" />
+            </DataTable.Col>
+            <DataTable.Col source="paid">
+                <BooleanField source="paid" />
+            </DataTable.Col>
+            <DataTable.NumberCol source="slary" />
+            <DataTable.Col source="salary">
+                <DateField source="salary" />
+            </DataTable.Col>
+        </DataTable>
+    </List>
+);
