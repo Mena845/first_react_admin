e4161637cbbf5a443c8069b639a98eaf85b3e5bf
Mena845 <tsikiniainatsilavina@gmail.com>
2026-05-30 08:19:34 +0300

    Test react admin

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Admin, Resource } from "react-admin";
+import { Layout } from "./Layout";
+import jsonServerProvider from "ra-data-json-server";
+import { EmployeeList } from "./employees/EmployeeList";
+import { EmployeeEdit } from "./employees/EmployeeEdit";
+import { EmployeeShow } from "./employees/EmployeeShow";
+import { EmployeeCreate } from "./employees/EmployeeCreate";
+
+const dataProvider = jsonServerProvider("http://localhost:3002");
+
+export const App = () => (
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource
+      name="employees"
+      list={EmployeeList}
+      create={EmployeeCreate}
+      edit={EmployeeEdit}
+      show={EmployeeShow}
+    />
+  </Admin>
+);
+
