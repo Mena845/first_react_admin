316270d11ae2a743b28123ed3599a406ec069e0e
Mena845 <tsikiniainatsilavina@gmail.com>
2026-06-03 23:39:55 +0300

    Refactor InterneList component to improve code organization and enhance filter labels for clarity

--- src/Internes/InterneList.tsx
+++ src/Internes/InterneList.tsx
@@ -1,45 +1,71 @@
-
-import { BooleanField, DataTable, DeleteButton, EditButton, EmailField,  List, NumberField, ReferenceField, ReferenceInput, SelectInput, TextField, TextInput } from 'react-admin';
+import {
+  BooleanField,
+  DataTable,
+  DeleteButton,
+  EditButton,
+  EmailField,
+  List,
+  NumberField,
+  ReferenceField,
+  SelectInput,
+  TextField,
+  TextInput,
+  TopToolbar,
+} from "react-admin";
+import { QuickAddInternModal } from "../Componenst/QuickAddInternModal";
 
 const internsFilters = [
-    <TextInput label="search" source="q" alwaysOn/>,
-    <SelectInput source="paid" label="Payé" choices={[
-        { id: 'true', name: 'Yes' },
-        { id: 'false', name: 'No' },
-    ]} />
+  <TextInput label="Recherche" source="q" alwaysOn />,
+  <SelectInput
+    source="paid"
+    label="Payé"
+    choices={[
+      { id: "true", name: "Oui" },
+      { id: "false", name: "Non" },
+    ]}
+  />,
 ];
+
+const ListActions = () => (
+  <TopToolbar>
+    <QuickAddInternModal />
+  </TopToolbar>
+);
+
 export const InterneList = () => (
-    <List filters={internsFilters}>
-        <DataTable>
-            <DataTable.Col source="id" />
-            <DataTable.Col source="firstName" />
-            <DataTable.Col source="lastName" />
-            <DataTable.Col source="email">
-                <EmailField source="email" />
-            </DataTable.Col>
-            <DataTable.Col source="mentorId" label="Mentor">
-    <ReferenceField source="mentorId" reference="Employees" link="show">
-        <TextField source="firstName" />
-    </ReferenceField>
-</DataTable.Col>
-                <DataTable.Col label='Departement'>
-                        <ReferenceField source='mentorId' reference='Departments'>
-                        <TextField source='name'/>
-                    </ReferenceField>
-                </DataTable.Col>
-            <DataTable.Col source="paid">
-    <BooleanField source="paid" />
-</DataTable.Col>
-            <DataTable.Col source="salary">
-                <NumberField source="salary" options={{ style: 'currency', currency: 'EUR' }} />
-            </DataTable.Col>
-             <DataTable.Col >
-             <EditButton />
-            </DataTable.Col>
-            <DataTable.Col >
-              <DeleteButton />
-            </DataTable.Col>
-        </DataTable>
-    </List>
-   
+  <List filters={internsFilters} actions={<ListActions />}>
+    <DataTable>
+      <DataTable.Col source="id" />
+      <DataTable.Col source="firstName" label="Prénom" />
+      <DataTable.Col source="lastName" label="Nom" />
+      <DataTable.Col source="email">
+        <EmailField source="email" />
+      </DataTable.Col>
+      <DataTable.Col source="mentorId" label="Manager">
+        <ReferenceField source="mentorId" reference="Employees" link="show">
+          <TextField source="firstName" />
+        </ReferenceField>
+      </DataTable.Col>
+      <DataTable.Col label="Département">
+        <ReferenceField source="mentorId" reference="Departments">
+          <TextField source="name" />
+        </ReferenceField>
+      </DataTable.Col>
+      <DataTable.Col source="paid" label="Payé">
+        <BooleanField source="paid" />
+      </DataTable.Col>
+      <DataTable.Col source="salary" label="Salaire">
+        <NumberField
+          source="salary"
+          options={{ style: "currency", currency: "EUR" }}
+        />
+      </DataTable.Col>
+      <DataTable.Col>
+        <EditButton />
+      </DataTable.Col>
+      <DataTable.Col>
+        <DeleteButton />
+      </DataTable.Col>
+    </DataTable>
+  </List>
 );
