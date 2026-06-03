78f56c91e484bd917e711db40910b71beab8b8ee
Mena845 <tsikiniainatsilavina@gmail.com>
2026-06-03 23:30:27 +0300

    Refactor InterneList component to replace ReferenceInput with ReferenceField and update BooleanInput to BooleanField for improved clarity and functionality

--- src/Internes/InterneList.tsx
+++ src/Internes/InterneList.tsx
@@ -1,5 +1,5 @@
 
-import { BooleanInput, DataTable, DeleteButton, EditButton, EmailField,  List, NumberField, ReferenceField, ReferenceInput, SelectInput, TextField, TextInput } from 'react-admin';
+import { BooleanField, DataTable, DeleteButton, EditButton, EmailField,  List, NumberField, ReferenceField, ReferenceInput, SelectInput, TextField, TextInput } from 'react-admin';
 
 const internsFilters = [
     <TextInput label="search" source="q" alwaysOn/>,
@@ -17,19 +17,19 @@ export const InterneList = () => (
             <DataTable.Col source="email">
                 <EmailField source="email" />
             </DataTable.Col>
-            <DataTable.Col source="mentorId">
-                <ReferenceInput source="mentorId" reference="Employees" label="Mentor" link="show">
-    <SelectInput optionText={(r) => `${r.firstName} ${r.lastName}`} />
-</ReferenceInput>
-            </DataTable.Col>
+            <DataTable.Col source="mentorId" label="Mentor">
+    <ReferenceField source="mentorId" reference="Employees" link="show">
+        <TextField source="firstName" />
+    </ReferenceField>
+</DataTable.Col>
                 <DataTable.Col label='Departement'>
                         <ReferenceField source='mentorId' reference='Departments'>
                         <TextField source='name'/>
                     </ReferenceField>
                 </DataTable.Col>
             <DataTable.Col source="paid">
-                <BooleanInput source="paid" label="Payé" defaultValue={false} />
-            </DataTable.Col>
+    <BooleanField source="paid" />
+</DataTable.Col>
             <DataTable.Col source="salary">
                 <NumberField source="salary" options={{ style: 'currency', currency: 'EUR' }} />
             </DataTable.Col>
