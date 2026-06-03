8d0f275510f9b379ef93ccba2344e1400f47850a
Mena845 <tsikiniainatsilavina@gmail.com>
2026-06-03 22:48:26 +0300

    Refactor EmployeeList component to streamline layout and remove unused DataTable references

--- src/employees/EmployeeList.tsx
+++ src/employees/EmployeeList.tsx
@@ -7,11 +7,9 @@ import {
   EditButton,
   DeleteButton,
   ReferenceField,
-  DataTable,
 } from "react-admin";
 import { employeesFilters } from "../Componenst/employeesFilters";
 import { QuickStatusToggle } from "../Componenst/QuickStatusToggle";
-
 
 export const EmployeeList = () => (
   <List filters={employeesFilters} perPage={5}>
@@ -19,21 +17,18 @@ export const EmployeeList = () => (
       <TextField source="firstName" label="Prénom" />
       <TextField source="lastName" label="Nom" />
       <TextField source="email" label="Email" />
-        <ReferenceField source="department" reference="Departments">
+      <ReferenceField source="department" reference="Departments">
         <TextField source="name" />
       </ReferenceField>
-    
       <NumberField
         source="salary"
         label="Salaire"
         options={{ style: "currency", currency: "EUR" }}
       />
       <BooleanField source="isActive" label="Actif" />
+      <QuickStatusToggle />
+      <EditButton />
+      <DeleteButton />
     </Datagrid>
-    <DataTable.Col label="Action">
-    <QuickStatusToggle />
-</DataTable.Col>
-    <EditButton />
-    <DeleteButton />
   </List>
 );
