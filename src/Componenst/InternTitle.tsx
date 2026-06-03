0d76057b2d112a7928e2f4ae8ca7fa1e52f2e1f9
Mena845 <tsikiniainatsilavina@gmail.com>
2026-06-03 23:42:21 +0300

    Refactor InternTitle component for improved readability and structure

--- src/Componenst/InternTitle.tsx
+++ src/Componenst/InternTitle.tsx
@@ -1,10 +1,11 @@
 import { useRecordContext } from "react-admin";
-export const InternTitle = () => {
-    const record = useRecordContext();
 
-    return record ? (
-        <span>
-            Modifier : {record.firstName} {record.lastName}
-        </span>
-    ) : null;
+export const InternTitle = () => {
+  const record = useRecordContext(); // ← obligatoire
+  if (!record) return null;
+  return (
+    <span>
+      Modifier : {record.firstName} {record.lastName}
+    </span>
+  );
 };
